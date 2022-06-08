0cc2cc794b8896e660205f2bdcac59a91d4262f4
iSIayer <gamerspeed86@gmail.com>
2022-06-08 20:54:16 +0300

    mentors correct

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,6 +12,7 @@ refs.submitBtn.addEventListener('click', evt => {
   createPromisesOnClickBtn(refs.amount.value);
 });
 
+// Создаем промис
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
@@ -25,12 +26,17 @@ function createPromise(position, delay) {
   });
 }
 
+// Создаем промисы и вызываем их по очереди
 function createPromisesOnClickBtn(amount) {
+  let timeout;
+  let step = parseInt(refs.step.value);
+  let firstDelay = parseInt(refs.delay.value);
   if (amount == 0) {
     alert("Amount can't be empty");
   } else {
     for (let i = 1; i <= amount; i += 1) {
-      createPromise(i, refs.delay.value)
+      i > 1 ? (timeout = firstDelay += step) : (timeout = firstDelay);
+      createPromise(i, timeout)
         .then(({ position, delay }) => {
           Notiflix.Notify.success(
             `✅ Fulfilled promise ${position} in ${delay}ms`
